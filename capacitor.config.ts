330843cf2b2154b806d4d494b8a02fd216a4948d
Sebas Echazú <sebastianechazu@outlook.com>
2025-07-03 14:51:42 -0300

    feat: add Manrope font styles and service worker for Angular application
    - Introduced a new CSS file for Manrope font styles, including various font weights and unicode ranges.
    - Implemented a basic service worker with install and activate event listeners to manage cache and unregister old service workers.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gymapp.app',
+  appName: 'gym-app',
+  webDir: 'www'
+};
+
+export default config;
