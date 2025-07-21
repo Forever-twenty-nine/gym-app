1bcb9d0603e48f6304a35e0b46976326ea5c6aae
Sebastian Echazu <sebastianechazu@outlook.com>
2025-07-21 12:31:01 -0300

    Refactor code structure for improved readability and maintainability

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.gymapp.app',
   appName: 'gym-app',
-  webDir: 'www'
+  webDir: 'www/browser',
 };
 
 export default config;
---
+++ www/browser/chunk-DMB64OWZ.js
@@ -0,0 +1,1 @@
+import{b as a,c as b}from"./chunk-6ZFCGJBU.js";import"./chunk-4H2XPKAU.js";export{a as GESTURE_CONTROLLER,b as createGesture};
